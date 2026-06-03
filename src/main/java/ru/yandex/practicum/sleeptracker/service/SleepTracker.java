3bcaed27991d768cd164a5d839488cb3f1b817c7
Skimmerzzz <skimmer@mail.ru>
2026-06-04 01:24:08 +0300

    Feature: - Implement exeption handling in SleepTracker - Implement SleepTrackerTest

--- src/main/java/ru/yandex/practicum/sleeptracker/service/SleepTracker.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/service/SleepTracker.java
@@ -7,10 +7,12 @@ import ru.yandex.practicum.sleeptracker.model.SleepingSession;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
@@ -34,35 +36,71 @@ public class SleepTracker {
         Path logFilePath = Paths.get(sleepLogFile);
 
         try (BufferedReader br = new BufferedReader(new FileReader(logFilePath.toFile()))) {
-            String line;
+            br.lines()
+                    .forEach(line -> {
+                                String[] lineTokens = line.split(";");
 
-            // TODO Don't use for/while loop
-            while ((line = br.readLine()) != null) {
-                String[] lineTokens = line.split(";");
+                                if (lineTokens.length == LOG_TOKENS_NUMBER) {
+                                    SleepingSession ss = null;
+                                    try {
+                                        ss = new SleepingSession(lineTokens[0],
+                                                lineTokens[1],
+                                                lineTokens[2],
+                                                LOG_DATETIME);
+                                    } catch (DateTimeParseException ex) {
+                                        System.out.println("Пропускаем строку. " +
+                                                "Неверный формат даты и времени: " +
+                                                lineTokens[0] + ", " +
+                                                lineTokens[1]);
+                                        return;
+                                    } catch (IllegalArgumentException ex) {
+                                        System.out.println("Пропускаем строку. " +
+                                                "Неверное качество сна: " +
+                                                lineTokens[2]);
+                                        return;
+                                    }
 
-                if (lineTokens.length == LOG_TOKENS_NUMBER) {
-                    // TODO Make constructor from whole line
-                    SleepingSession ss = new SleepingSession(lineTokens[0], lineTokens[1], lineTokens[2], LOG_DATETIME);
-                    // TODO Session end have to be greater than start
-                    // TODO At least one session
-                    // TODO Check: is session start already exits?
-                    sleepingSessionJournal.put(ss.getSessionStart(), ss);
-                } else {
-                    System.out.println("Неверная структура лога: количество частей строки не равно" + LOG_TOKENS_NUMBER);
-                    continue;
-                }
-            }
-        } catch (IllegalArgumentException ex) {
-            System.out.println(ex.getMessage());
-            ex.printStackTrace();
+                                    // Session end have to be greater than start
+                                    if (ss.getSessionEnd().isBefore(ss.getSessionStart())
+                                            || ss.getSessionEnd().isEqual(ss.getSessionStart())) {
+                                        System.out.println("Пропускаем строку. " +
+                                                "Время окончания сессии " +
+                                                ss.getSessionEnd() +
+                                                " должно быть больше начала " +
+                                                ss.getSessionStart());
+                                    } else {
+                                        // Is session start already exits?
+                                        if (sleepingSessionJournal.containsKey(ss.getSessionStart())) {
+                                            System.out.println("Пропускаем строку. " +
+                                                    "Уже есть сессия с таким временем начала " +
+                                                    ss.getSessionStart());
+                                        } else {
+                                            sleepingSessionJournal.put(ss.getSessionStart(), ss);
+                                        }
+                                    }
+                                } else {
+                                    System.out.println("Пропускаем строку. " +
+                                            "Неверная структура лога: количество частей строки не равно " +
+                                            LOG_TOKENS_NUMBER);
+                                }
+                            }
+                    );
+        } catch (InvalidPathException ex) {
+            System.out.println("Неверный путь к файлу" + logFilePath + ". " + ex.getMessage());
+            System.exit(1);
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
-            ex.printStackTrace();
+            System.exit(1);
+        }
+
+        // At least one session
+        if (sleepingSessionJournal.isEmpty()) {
+            System.out.println("Нет записей в журнале сессий" + logFilePath);
+            System.exit(1);
         }
     }
 
     public List<SleepAnalysisResult<?>> getAnalytics() {
-//        TODO run functions here
         return analyticFunctions.stream()
                 .map(func -> func.get(sleepingSessionJournal))
                 .collect(Collectors.toList());
--- src/test/java/ru/yandex/practicum/sleeptracker/service/SleepTrackerTest.java
+++ src/test/java/ru/yandex/practicum/sleeptracker/service/SleepTrackerTest.java
@@ -1,32 +1,96 @@
 package ru.yandex.practicum.sleeptracker.service;
 
 import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.sleeptracker.functions.*;
+import ru.yandex.practicum.sleeptracker.model.SleepAnalysisResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SleepTrackerTest {
 
     @Test
-    void initWrongFilePath() {
-    }
-
-    @Test
     void initWrongDateTimeFormat() {
+        ArrayList<SleepingAnalysis> analyticFunctions = new ArrayList<>();
+        analyticFunctions.add(new SessionNumberAnalysis());
+        SleepTracker st = new SleepTracker("./src/test/resources/sleep_log_wrong_datetime.txt",
+                analyticFunctions);
+        st.init();
+
+        List<SleepAnalysisResult<?>> results = st.getAnalytics();
+        if (results.getFirst().getOrEmpty().isPresent()) {
+            assertEquals(1,results.getFirst().getOrEmpty().get());
+        } else {
+            fail();
+        }
     }
 
     @Test
     void initWrongSleepQuality() {
+        ArrayList<SleepingAnalysis> analyticFunctions = new ArrayList<>();
+        analyticFunctions.add(new SessionNumberAnalysis());
+        SleepTracker st = new SleepTracker("./src/test/resources/sleep_log_wrong_quality.txt",
+                analyticFunctions);
+        st.init();
+
+        List<SleepAnalysisResult<?>> results = st.getAnalytics();
+        if (results.getFirst().getOrEmpty().isPresent()) {
+            assertEquals(1,results.getFirst().getOrEmpty().get());
+        } else {
+            fail();
+        }
     }
 
     @Test
-    void initEmptyLine() {
+    void initSameSessionStartTime() {
+        ArrayList<SleepingAnalysis> analyticFunctions = new ArrayList<>();
+        analyticFunctions.add(new SessionNumberAnalysis());
+        SleepTracker st = new SleepTracker("./src/test/resources/sleep_log_check_doubles.txt",
+                analyticFunctions);
+        st.init();
+
+        List<SleepAnalysisResult<?>> results = st.getAnalytics();
+        if (results.getFirst().getOrEmpty().isPresent()) {
+            assertEquals(1,results.getFirst().getOrEmpty().get());
+        } else {
+            fail();
+        }
     }
 
     @Test
-    void initNotEnoughInfoInLine() {
+    void initSkipIncorrectLines() {
+        ArrayList<SleepingAnalysis> analyticFunctions = new ArrayList<>();
+        analyticFunctions.add(new SessionNumberAnalysis());
+        SleepTracker st = new SleepTracker("./src/test/resources/sleep_log_wrong_lines_one_correct.txt",
+                analyticFunctions);
+        st.init();
+
+        List<SleepAnalysisResult<?>> results = st.getAnalytics();
+        if (results.getFirst().getOrEmpty().isPresent()) {
+            assertEquals(1,results.getFirst().getOrEmpty().get());
+        } else {
+            fail();
+        }
     }
 
     @Test
     void getAnalytics() {
+        ArrayList<SleepingAnalysis> analyticFunctions = new ArrayList<>();
+        analyticFunctions.add(new SessionNumberAnalysis());
+        analyticFunctions.add(new SessionMaxDurationAnalysis());
+        analyticFunctions.add(new SessionMinDurationAnalysis());
+        analyticFunctions.add(new SessionAvgDurationAnalysis());
+        analyticFunctions.add(new SessionBadQualityCounterAnalysis());
+        analyticFunctions.add(new UserChronotypeAnalysis());
+        analyticFunctions.add(new SessionSleeplessNightsAnalysis());
+
+        SleepTracker st = new SleepTracker("./src/test/resources/sleep_log.txt",
+                analyticFunctions);
+        st.init();
+
+        List<SleepAnalysisResult<?>> results = st.getAnalytics();
+        assertEquals(7, results.size());
     }
 }
